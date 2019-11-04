4696f946734c56e4b97a9284f66166e06167554d
RavenKite <lipeizhuo0528@outlook.com>
2019-11-04 15:20:24 +0800

    Initial commit

---
+++ RKObjcTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RKObjcTest
+//
+//  Created by 李沛倬 on 2019/11/4.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
